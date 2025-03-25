01ae640952666a525b4c3de79454ebb3cef861be
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2025-03-25 15:52:10 +0600

    IntersectionObserver

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,18 @@
 document.querySelector(".toggler").addEventListener("click", function () {
     document.getElementById("navBar").classList.toggle("nav-expanded");
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const skillContainer = document.querySelector("#skill-container");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                skillContainer.classList.add("skill-active"); // Add class when in view
+            } else {
+                skillContainer.classList.remove("skill-active"); // Remove class when out of view
+            }
+        });
+    }, { threshold: 0.3 });
+
+    observer.observe(skillContainer);
+});
