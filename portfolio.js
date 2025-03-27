c98707d08e81eee1c3e6e9d886be03116dc930db
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2025-03-27 10:50:42 +0600

    Dunno

--- portfolio.js
+++ portfolio.js
@@ -16,3 +16,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     observer.observe(skillContainer);
 });
+
+//slider
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: { delay: 1000 },
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+});
